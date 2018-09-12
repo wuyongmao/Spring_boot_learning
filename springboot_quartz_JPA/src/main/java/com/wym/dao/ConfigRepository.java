eab561efd4fdc287ae9447aba6ac99f935e385b2
wuyongmao <741262164@qq.com>
2018-09-12 21:29:00 +0800

    删除了springboot_quartz_JPA

--- springboot_quartz_JPA/src/main/java/com/wym/dao/ConfigRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wym.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wym.Entity.Config;
-
-public interface ConfigRepository extends  JpaRepository  <Config, Integer> {
-
-}
--- springboot_quartz_JPA/src/main/resources/data.sql
+++
@@ -1,1 +0,0 @@
-insert into config(id,cron) values(1,'0 0/2 * * * ?');
