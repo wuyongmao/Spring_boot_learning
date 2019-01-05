4f502ab6369498e6454f77d097fbb030d775f6db
wuyongmao <741262164@qq.com>
2019-01-05 16:33:31 +0800

    Create CrosConfigFilter.java
    配置全局CROS跨域

---
+++ springboot_JPA/src/main/java/com/wym/springboot01/config/CrosConfigFilter.java
@@ -0,0 +1,42 @@
+package com.wym.springboot01.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * 
+ * 全局CROS跨域， 或者Controller中添加
+ * 
+ * @CrossOrigin(origins = "http://172.16.0.59:8080", maxAge = 3600)
+ * @author yongmaow
+ */
+@Configuration
+public class CrosConfigFilter {
+
+	@Bean
+	public FilterRegistrationBean crosFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+		CorsConfiguration cros = new CorsConfiguration();
+
+		cros.setAllowCredentials(false);
+
+		cros.addAllowedOrigin("*");
+
+		cros.addAllowedHeader("*");
+
+		cros.addAllowedMethod("*");
+
+		source.registerCorsConfiguration("/**", cros);
+
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+
+		bean.setOrder(0);
+
+		return bean;
+	}
+}
